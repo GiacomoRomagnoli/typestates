56c09c78b1d2818df330569eccc53e2389f737fb
Giacomo Romagnoli <giacomo.romagnoli4@studio.unibo.it>
2026-05-18 17:00:59 +0200

    refactor[processor]: apply the single responsibility principle to CheckerScope.kt moving utilities in Extensions.kt

--- processor/src/main/kotlin/dsl/CheckerScope.kt
+++ processor/src/main/kotlin/dsl/CheckerScope.kt
@@ -1,8 +1,13 @@
 package dsl
 
-import ast.DecisionTargetNode
+import ast.OutPutStateNode
 import ast.MethodNode
-import ast.TypeNode
+import ast.JavaTypeNode
+import processor.allMeths
+import processor.allRt
+import processor.match
+import processor.protocol
+import processor.toJavaType
 import semantic.Protocol.labels
 import semantic.Protocol.parse
 import semantic.Protocol.protIn
@@ -20,92 +25,38 @@ import javax.tools.StandardLocation
 
 class CheckerScope(val context: ProcessingEnvironment) {
 
-    fun TypeElement.allMethods() : List<ExecutableElement> {
-        val methods = enclosedElements.filter { it.kind == ElementKind.METHOD }.map { it as ExecutableElement }
-        return when (superclass) {
-            is NoType -> methods
-            else -> methods + (context.typeUtils.asElement(superclass) as TypeElement).allMethods()
-                .filterNot { sm -> methods.any { context.elementUtils.overrides(it, sm, this) } }
-        }
-    }
-
-    fun TypeNode.toJavaType(): TypeMirror {
-        val qualified = name.joinToString(".") { it.value }
-        var type = when (qualified) {
-            "byte" -> context.typeUtils.getPrimitiveType(TypeKind.BYTE)
-            "short" -> context.typeUtils.getPrimitiveType(TypeKind.SHORT)
-            "int" -> context.typeUtils.getPrimitiveType(TypeKind.INT)
-            "long" -> context.typeUtils.getPrimitiveType(TypeKind.LONG)
-            "float" -> context.typeUtils.getPrimitiveType(TypeKind.FLOAT)
-            "double" -> context.typeUtils.getPrimitiveType(TypeKind.DOUBLE)
-            "boolean" -> context.typeUtils.getPrimitiveType(TypeKind.BOOLEAN)
-            "char" -> context.typeUtils.getPrimitiveType(TypeKind.CHAR)
-            else -> context.elementUtils.getTypeElement(qualified)?.asType()
-        } ?: throw SemanticException(
-            "type $qualified at ${position.startLineCol} " +
-            "in ${position.filename} is not a valid Java type"
-        )
-        repeat(arrayLevel) {
-            type = context.typeUtils.getArrayType(type)
-        }
-        return type
-    }
-
-    infix fun ExecutableElement.match(protocolMethod: MethodNode): Boolean {
-        if (!simpleName.contentEquals(protocolMethod.name.value))
-            return false
-        if (parameters.size != protocolMethod.args.size)
-            return false
-        return parameters
-            .map { it.asType() }
-            .zip(protocolMethod.args)
-            .all { (jt, pt) -> context.typeUtils.isSameType(jt, pt.toJavaType()) }
-    }
-
-    fun TypeElement.allRt(m: ExecutableElement): Set<String>? =
-        when (m.returnType.kind) {
-            TypeKind.BOOLEAN -> setOf("true", "false")
-            TypeKind.DECLARED -> when (context.typeUtils.asElement(m.returnType).kind) {
-                ElementKind.ENUM -> context.typeUtils.asElement(m.returnType).enclosedElements
-                    .filter { it.kind == ElementKind.ENUM_CONSTANT }
-                    .map { it.simpleName.toString() }
-                    .toSet()
-                else -> null
-            }
-            else -> null
-        }
-
-    fun chkProt(clazz: TypeElement) {
-        val path = clazz.getAnnotation(Typestate::class.java).value
-        val ps = context.filer.getResource(StandardLocation.CLASS_PATH, "", path).getCharContent(false).toString()
-        val protocol = parse(ps)
-        protocol.validate()
-        val classMethods = clazz.allMethods()
+    fun chkProt(clazz: TypeElement): Boolean {
+        val protocol = context.protocol(clazz)
+        val classMethods = context.allMeths(clazz)
+        var holds = true
         for (transition in protocol.protIn().flatMap { it.transitions }) {
-                val method = classMethods.find { it match transition.method }
-                if (method == null) {
-                    context.messager.printMessage(
-                        Diagnostic.Kind.ERROR,
-                        "method ${transition.method.name.value} at " +
-                        "${transition.method.position.startLineCol} in " +
-                        "${protocol.position.filename} has not been declared"
-                    )
-                } else when (val w = transition.target) {
-                    is DecisionTargetNode -> {
-                        val expected = clazz.allRt(method)
-                        val actual = w.labels()
-                        if (expected != actual) {
-                            context.messager.printMessage(
-                                Diagnostic.Kind.ERROR,
-                                "output state at ${w.position.startLineCol} in " +
-                                "${protocol.position.filename} is not exhaustive. " +
-                                "expected=$expected actual=$actual"
-                            )
-                        }
+            val method = classMethods.find { context.match(it, transition.method) }
+            if (method == null) {
+                holds = false
+                context.messager.printMessage(
+                    Diagnostic.Kind.ERROR,
+                    "method ${transition.method.name.value} at "
+                            + "${transition.method.position.startLineCol} in "
+                            + "${protocol.position.filename} has not been declared"
+                )
+            } else when (val w = transition.target) {
+                is OutPutStateNode -> {
+                    val expected = context.allRt(method)
+                    val actual = w.labels()
+                    if (expected != actual) {
+                        holds = false
+                        context.messager.printMessage(
+                            Diagnostic.Kind.ERROR,
+                            "output state at ${w.position.startLineCol} in "
+                                    + "${protocol.position.filename} is not exhaustive. "
+                                    + "expected=$expected actual=$actual"
+                        )
                     }
-                    else -> Unit
                 }
+                else -> Unit
+            }
         }
+        return holds
     }
 
     companion object {
---
+++ processor/src/main/kotlin/processor/Extensions.kt
@@ -0,0 +1,79 @@
+package processor
+
+import ast.JavaTypeNode
+import ast.MethodNode
+import ast.ProtocolNode
+import semantic.Protocol.parse
+import semantic.Protocol.validate
+import javax.annotation.processing.ProcessingEnvironment
+import javax.lang.model.element.ElementKind
+import javax.lang.model.element.ExecutableElement
+import javax.lang.model.element.TypeElement
+import javax.lang.model.type.NoType
+import javax.lang.model.type.TypeKind
+import javax.lang.model.type.TypeMirror
+import javax.tools.StandardLocation
+import kotlin.collections.plus
+
+fun ProcessingEnvironment.protocol(clazz: TypeElement) : ProtocolNode {
+    val path = clazz.getAnnotation(Typestate::class.java)?.value
+    val ps = filer.getResource(
+        StandardLocation.CLASS_PATH,
+        "",
+        path
+    ).getCharContent(false).toString()
+    val protocol = parse(ps)
+    return protocol.validate()
+}
+
+fun ProcessingEnvironment.allMeths(clazz: TypeElement) : List<ExecutableElement> {
+    val methods = clazz.enclosedElements.filter { it.kind == ElementKind.METHOD }.map { it as ExecutableElement }
+    return when (clazz.superclass) {
+        is NoType -> methods
+        else -> methods + allMeths((typeUtils.asElement(clazz.superclass) as TypeElement))
+            .filterNot { sm -> methods.any { elementUtils.overrides(it, sm, clazz) } }
+    }
+}
+
+fun ProcessingEnvironment.toJavaType(node: JavaTypeNode): TypeMirror? {
+    val qualified = node.name.joinToString(".") { it.value }
+    var type = when (qualified) {
+        "byte" -> typeUtils.getPrimitiveType(TypeKind.BYTE)
+        "short" -> typeUtils.getPrimitiveType(TypeKind.SHORT)
+        "int" -> typeUtils.getPrimitiveType(TypeKind.INT)
+        "long" -> typeUtils.getPrimitiveType(TypeKind.LONG)
+        "float" -> typeUtils.getPrimitiveType(TypeKind.FLOAT)
+        "double" -> typeUtils.getPrimitiveType(TypeKind.DOUBLE)
+        "boolean" -> typeUtils.getPrimitiveType(TypeKind.BOOLEAN)
+        "char" -> typeUtils.getPrimitiveType(TypeKind.CHAR)
+        else -> elementUtils.getTypeElement(qualified)?.asType()
+    }
+    repeat(node.arrayLevel) {
+        type = typeUtils.getArrayType(type)
+    }
+    return type
+}
+
+fun ProcessingEnvironment.match(jm: ExecutableElement, pm: MethodNode): Boolean {
+    if (!jm.simpleName.contentEquals(pm.name.value))
+        return false
+    if (jm.parameters.size != pm.args.size)
+        return false
+    return jm.parameters
+        .map { it.asType() }
+        .zip(pm.args)
+        .all { (jt, pt) -> typeUtils.isSameType(jt, toJavaType(pt)) }
+}
+
+fun ProcessingEnvironment.allRt(m: ExecutableElement): Set<String>? =
+    when (m.returnType.kind) {
+        TypeKind.BOOLEAN -> setOf("true", "false")
+        TypeKind.DECLARED -> when (typeUtils.asElement(m.returnType).kind) {
+            ElementKind.ENUM -> typeUtils.asElement(m.returnType).enclosedElements
+                .filter { it.kind == ElementKind.ENUM_CONSTANT }
+                .map { it.simpleName.toString() }
+                .toSet()
+            else -> null
+        }
+        else -> null
+    }
