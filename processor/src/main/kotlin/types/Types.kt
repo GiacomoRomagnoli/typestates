9ea4959c6b63efd937cfdcb76f8a059a552b51da
Giacomo Romagnoli <giacomo.romagnoli4@studio.unibo.it>
2026-06-01 17:50:05 +0200

    feat[processor]: add End type and sub predicate

--- processor/src/main/kotlin/types/Types.kt
+++ processor/src/main/kotlin/types/Types.kt
@@ -12,6 +12,24 @@ data object Bottom: Type
 data object Shared: Type
 data object Null: Type
 data object Und: Type
+data object End: Type
 
 infix fun Type.or(other: Type) = Intersection(this, other)
 infix fun Type.and(other: Type) = Union(this, other)
+infix fun Type.sub(other: Type): Boolean = when {
+    this is Bottom -> true
+    other is Top -> true
+    this is Union -> this.t1 sub other && this.t2 sub other
+    this is Intersection -> this.t1 sub other || this.t2 sub other
+    other is Union -> this sub other.t1 || this sub other.t2
+    other is Intersection -> this sub other.t1 && this sub other.t2
+    this is Shared -> other is Shared
+    this is Null -> other is Null
+    this is Und -> other is Und
+    this is U -> when (other) {
+        is End, is Shared, is Null, is Und -> this.state.isEnd()
+        is U -> this.state.simulates(other.state)
+        else -> false
+    }
+    else -> false
+}
