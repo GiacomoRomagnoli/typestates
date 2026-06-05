b3b40487b76f877afd86bd03c3f71dbcbab299e7
Giacomo Romagnoli <giacomo.romagnoli4@studio.unibo.it>
2026-06-05 14:46:15 +0200

    chore[processor]: remove typo

--- processor/src/main/kotlin/types/QualifiedType.kt
+++ processor/src/main/kotlin/types/QualifiedType.kt
@@ -8,5 +8,5 @@ data class QualifiedType(val c: Class, val t: Type)
 infix fun QualifiedType.sub(other: QualifiedType) =
     protIn(c).containsAll(typestates(t))
             && protIn(other.c).containsAll(typestates(other.t))
-            && Java.types.isSubtype(c.element.asType(), other.c.element.asType()) == true
+            && Java.types.isSubtype(c.element.asType(), other.c.element.asType())
             && t sub other.t
