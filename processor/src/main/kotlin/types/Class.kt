b87cc70fb74234e33498e48d460cffc2844cb04e
Giacomo Romagnoli <giacomo.romagnoli4@studio.unibo.it>
2026-06-12 17:41:37 +0200

    refactor[processor]: refactor for allMeths and meths

--- processor/src/main/kotlin/types/Class.kt
+++ processor/src/main/kotlin/types/Class.kt
@@ -1,16 +1,22 @@
 package types
 
 import processor.environment.Java
-import processor.environment.allMeths
 import processor.environment.linearClassOf
+import javax.lang.model.element.ElementKind
+import javax.lang.model.element.ExecutableElement
 import javax.lang.model.element.TypeElement
+import kotlin.collections.plus
 
 sealed interface Class {
     val element: TypeElement
 }
 
-fun allMeths(c: Class) = Java.env.allMeths(c.element)
-// TODO Object è suprclasse di tutte le classi e viene considerata come tale
+fun allMeths(c: Class): List<ExecutableElement> = when(val supC = sup(c)) {
+    c -> meths(c)
+    else -> meths(c) + allMeths(supC)
+}
+fun meths(c: Class) =
+    c.element.enclosedElements.filter { it.kind == ElementKind.METHOD }.map { it as ExecutableElement }
 fun sup(c: Class) =
     (Java.types.asElement(c.element.superclass) as? TypeElement)?.let {
         if (it.getAnnotation(Typestate::class.java) != null) {
