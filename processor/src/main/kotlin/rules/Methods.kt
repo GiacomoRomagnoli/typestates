b063858631c7cab8c56d186410114fa37c28be84
Giacomo Romagnoli <giacomo.romagnoli4@studio.unibo.it>
2026-08-14 11:54:15 +0200

    test: add TMeth2

--- processor/src/main/kotlin/rules/Methods.kt
+++ processor/src/main/kotlin/rules/Methods.kt
@@ -61,4 +61,40 @@ val METHOD_JUDGEMENT: Judgement<Meth.Left, TypeEnv> = judgement {
             right { it }
         }
     }
+
+    rule("TMeth2") {
+        premise {
+            val variables: TypeEnv = buildMap {
+                put(THIS, tt(c, Shared))
+                method.pt.forEach { put(it.name, toTC(it.type)) }
+            }
+            val breakFields = fields.bottom()
+            val returnFields = fields.bottom()
+            val breakVariables = variables.bottom()
+            val body = method.body ?: fail()
+            val statements = (body.leaf as BlockTree).statements.map { TreePath(body, it) }
+            val derivation = STATEMENT_SEQUENCE_JUDGMENT.derive(
+                StmtSeq.Left(
+                    fields = fields,
+                    variables = variables,
+                    breakFields = breakFields,
+                    breakVariables = breakVariables,
+                    returnFields = returnFields,
+                    statements = statements,
+                    returnType = method.rt,
+                    program = program,
+                    f = false
+                )
+            )
+            ensure(derivation.fields == derivation.fields.bottom())
+            ensure(derivation.variables == derivation.variables.bottom())
+            ensure(derivation.breakFields == breakFields)
+            ensure(derivation.breakVariables == derivation.breakVariables.bottom())
+            derivation.returnFields
+        }
+        conclusion {
+            left { method.rt != Void}
+            right { it }
+        }
+    }
 }
